bcb5dab9bb6cfd592a287c3f5421c19e4b0d5a88
esot321c <esot321c@gmail.com>
2023-03-27 13:07:39 -0700

    select token cards with checkboxes added

--- components/NftCard.tsx
+++ components/NftCard.tsx
@@ -1,23 +1,20 @@
 import React, { FC, useEffect, useMemo, useState } from 'react';
 import {
   Card,
-  CardMedia,
   CardContent,
   CardActions,
   CardActionArea,
-  Button,
   Box,
   Typography,
-  useTheme
+  useTheme,
 } from '@mui/material'
 import dynamic from 'next/dynamic'
 import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
 import Image from 'next/image';
 import Link from '@components/Link';
-import { ThemeContext } from '@emotion/react';
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import { useRouter } from 'next/router'
-import OpenPacks from '@components/dialogs/OpenPacks';
+import { styled } from '@mui/material/styles';
+import Checkbox, { CheckboxProps } from '@mui/material/Checkbox';
 // const TimeRemaining = dynamic(() => import('@components/TimeRemaining'), {
 //   ssr: false,
 // });
@@ -86,17 +83,29 @@ const NftCard: FC<INftCard> = ({
   return (
     <>
       <Card
-        elevation={0}
         sx={{
           // minWidth: '276px',
-          borderColor: selected !== undefined && index !== undefined && selected[index] ? '#f00' : theme.palette.divider,
+          backgroundColor: selected !== undefined && index !== undefined && selected[index] ?
+            theme.palette.divider :
+            theme.palette.background.paper,
+          // boxShadow: selected !== undefined && index !== undefined && selected[index] ?
+          //   `0 0 3px 0px ${theme.palette.primary.main}` :
+          //   'none',
           mb: '6px',
           height: '100%',
+          transform: selected !== undefined && index !== undefined && selected[index] ?
+            "scale3d(0.95, 0.95, 1)" :
+            "scale3d(1, 1, 1)",
+          transition: "transform 0.15s ease-in-out"
         }}
       >
         <CardActionArea
           href={setSelected === undefined ? nftData.link : ''}
           onClick={() => setSelected != undefined && handleSelect()}
+          disableRipple={selected !== undefined && index !== undefined ?
+            true :
+            false
+          }
         >
           <Box sx={{
             position: 'relative',
@@ -131,6 +140,17 @@ const NftCard: FC<INftCard> = ({
                 {nftData.price + ' ' + nftData.currency}
               </Typography>
             </Box>
+          )}
+          {selected !== undefined && index !== undefined && (
+            <BpCheckbox
+              sx={{
+                position: 'absolute',
+                top: 1,
+                left: 1,
+              }}
+              checked={selected[index] !== undefined ? selected[index] : false}
+              inputProps={{ 'aria-label': 'selected-nft' }}
+            />
           )}
           <CardContent sx={{ position: 'relative' }}>
             {nftData.saleType && setSelected === undefined && (
@@ -259,5 +279,65 @@ const NftCard: FC<INftCard> = ({
     </>
   );
 };
+
+function BpCheckbox(props: CheckboxProps) {
+  return (
+    <Checkbox
+      sx={{
+        '&:hover': { bgcolor: 'transparent' },
+      }}
+      disableRipple
+      color="default"
+      checkedIcon={<BpCheckedIcon />}
+      icon={<BpIcon />}
+      {...props}
+    />
+  );
+}
+
+const BpIcon = styled('span')(({ theme }) => ({
+  borderRadius: 3,
+  width: 20,
+  height: 20,
+  boxShadow:
+    theme.palette.mode === 'dark'
+      ? '0 0 0 1px rgb(16 22 26 / 40%)'
+      : 'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
+  backgroundColor: theme.palette.mode === 'dark' ? '#394b59' : '#f5f8fa',
+  backgroundImage:
+    theme.palette.mode === 'dark'
+      ? 'linear-gradient(180deg,hsla(0,0%,100%,.05),hsla(0,0%,100%,0))'
+      : 'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
+  '.Mui-focusVisible &': {
+    outline: '2px auto rgba(19,124,189,.6)',
+    outlineOffset: 2,
+  },
+  'input:hover ~ &': {
+    backgroundColor: theme.palette.mode === 'dark' ? '#30404d' : '#ebf1f5',
+  },
+  'input:disabled ~ &': {
+    boxShadow: 'none',
+    background:
+      theme.palette.mode === 'dark' ? 'rgba(57,75,89,.5)' : 'rgba(206,217,224,.5)',
+  },
+}));
+
+const BpCheckedIcon = styled(BpIcon)(({ theme }) => ({
+  backgroundColor: '#00868F',
+  backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
+  '&:before': {
+    display: 'block',
+    width: 20,
+    height: 20,
+    backgroundImage:
+      "url(\"data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Cpath" +
+      " fill-rule='evenodd' clip-rule='evenodd' d='M12 5c-.28 0-.53.11-.71.29L7 9.59l-2.29-2.3a1.003 " +
+      "1.003 0 00-1.42 1.42l3 3c.18.18.43.29.71.29s.53-.11.71-.29l5-5A1.003 1.003 0 0012 5z' fill='%23fff'/%3E%3C/svg%3E\")",
+    content: '""',
+  },
+  'input:hover ~ &': {
+    backgroundColor: '#106ba3',
+  },
+}));
 
 export default NftCard;
