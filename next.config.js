d9df74b53e9f7f1ec6a99e1fa9c0c39155d0c9de
noob77777 <abhishekpal7@outlook.com>
2023-04-07 15:05:07 +0530

    :update: config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
     API_URL: process.env.API_URL,
     ERGOPAD_API: process.env.ERGOPAD_API,
   },
+  images: {
+    domains: ['ergopad-public.s3.us-west-2.amazonaws.com', 'cloudflare-ipfs.com'],
+  },
   swcMinify: true,
 };
 
